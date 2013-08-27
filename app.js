77d7a8a0daec7ebabccac2ef796333feb84d2059
Kyle Tolle <kyle@nullsix.com>
2013-08-27 08:07:23 -0600

    Change host and port for production

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
-app.listen(8000);
+app.listen(80);
 
 function handler (req, res) {
   path = req.url;
