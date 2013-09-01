1df4227aaf89a6ae8ff3192bc993d0e90dd7922b
Kyle Tolle <kyle@nullsix.com>
2013-08-31 21:18:35 -0600

    Refactor out methods; use JS-style names

--- client.js
+++ client.js
@@ -1,27 +1,28 @@
 var socket = io.connect('http://fueledby.us');
-var name_has_been_set = false;
+var nameHasBeenSet = false;
 
 socket.on('download', function (data) {
-    console.log("Received broadcast: " + data);
-    $('#content').html(data);
+  $('#content').html(data);
 });
 
 socket.on('show names', function(names) {
-  console.log("received show names");
-    if(name_has_been_set == true) {
-      $('#name').html('<p><b>Users:</b> ' + names + '</p>');
-    }
+  if(nameHasBeenSet == true) {
+    $('#name').html('<p><b>Users:</b> ' + names + '</p>');
+  }
 });
 
 $(document).ready(function() {
-  $('#content').keyup(function() {
-    socket.emit('upload', $('#content').html());
-  });
-
-  $('#username').keyup(function(event) {
-    if (event.keyCode == 13) {
-      socket.emit('set name', $('#username').val());
-      name_has_been_set = true;
-    }
-  });
+  $('#content').keyup(uploadContent);
+  $('#username').keyup(processUsername);
 });
+
+var uploadContent = function() {
+    socket.emit('upload', $('#content').html());
+};
+
+var processUsername = function() {
+  if (event.keyCode == 13) {
+    socket.emit('set name', $('#username').val());
+    nameHasBeenSet = true;
+  }
+};
